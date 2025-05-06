017b6bc696b190a54acc3542de882f395f3227ad
Dennis Tereshkow <tereshkowdennis@gmail.com>
2025-05-06 18:52:23 +0300

    pep fixing

--- src/outputs.py
+++ src/outputs.py
@@ -4,10 +4,9 @@ import datetime as dt
 
 from prettytable import PrettyTable
 
-# Дополнительно импортируйте формат даты-времени.
 from constants import BASE_DIR, DATETIME_FORMAT
 
-# Контроль вывода результатов парсинга.
+
 def control_output(results, cli_args):
     output = cli_args.output
     if output == 'pretty':
@@ -17,12 +16,12 @@ def control_output(results, cli_args):
     else:
         default_output(results)
 
-# Вывод данных в терминал построчно.
+
 def default_output(results):
     for row in results:
-        print(*row)    
+        print(*row)
 
-# Вывод данных в формате PrettyTable.
+
 def pretty_output(results):
     table = PrettyTable()
     table.field_names = results[0]
@@ -30,29 +29,16 @@ def pretty_output(results):
     table.add_rows(results[1:])
     print(table)
 
-# Создание директории с результатами парсинга. 
+
 def file_output(results, cli_args):
     results_dir = BASE_DIR / 'results'
     results_dir.mkdir(exist_ok=True)
-    # Отсюда начинается новый код. 
-    # Получаем режим работы парсера из аргументов командной строки.
     parser_mode = cli_args.mode
-    # Получаем текущие дату и время.
     now = dt.datetime.now()
-    # Сохраняем текущие дату-время в указанном формате. 
-    # Результат будет выглядеть вот так: 2021-06-18_07-40-41.
     now_formatted = now.strftime(DATETIME_FORMAT)
-    # Собираем имя файла из полученных переменных: 
-    # «режим работы программы» + «дата и время записи» + формат (.csv).
     file_name = f'{parser_mode}_{now_formatted}.csv'
-    # Получаем абсолютный путь к файлу с результатами.
-    file_path = results_dir / file_name 
-        # Отсюда начинается новый код.
-    # Через контекстный менеджер открываем файл по сформированному ранее пути 
-    # в режиме записи 'w', в нужной кодировке utf-8.
+    file_path = results_dir / file_name
     with open(file_path, 'w', encoding='utf-8') as f:
-        # Создаём «объект записи» writer.
         writer = csv.writer(f, dialect='unix')
-        # Передаём в метод writerows список с результатами парсинга.
         writer.writerows(results)
     logging.info(f'Файл с результатами был сохранён: {file_path}')
